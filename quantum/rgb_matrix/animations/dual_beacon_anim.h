54f17cbab2700843b707b8c1cee3e01d2e8c0559
vNmven0m <danielbruch631@mail.com>
2023-12-12 13:41:35 +0100

    Custom Color Cycle Effect

--- quantum/rgb_matrix/animations/dual_beacon_anim.h
+++ quantum/rgb_matrix/animations/dual_beacon_anim.h
@@ -3,24 +3,7 @@ RGB_MATRIX_EFFECT(DUAL_BEACON)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
 static HSV DUAL_BEACON_math(HSV hsv, int8_t sin, int8_t cos, uint8_t i, uint8_t time) {
-    float beacon_effect = ((g_led_config.point[i].y - k_rgb_matrix_center.y) * cos + (g_led_config.point[i].x - k_rgb_matrix_center.x) * sin) / 128;
-
-    hsv.h += beacon_effect;
-
-    // Background color
-    HSV background_color;
-    background_color.h = 190;  // Hue for lightblue
-    background_color.s = 255;  // Saturation
-    background_color.v = 255;  // Brightness
-
-    // Adjust the blending factor based on the beacon effect
-    float blend_factor = 0.7;  // Adjust this value based on the desired blend
-
-    // Blend the background color with the beacon effect
-    hsv.h = (1 - blend_factor) * background_color.h + blend_factor * (hsv.h - beacon_effect);
-    hsv.s = (1 - blend_factor) * background_color.s + blend_factor * hsv.s;
-    hsv.v = (1 - blend_factor) * background_color.v + blend_factor * hsv.v;
-
+    hsv.h += ((g_led_config.point[i].y - k_rgb_matrix_center.y) * cos + (g_led_config.point[i].x - k_rgb_matrix_center.x) * sin) / 128;
     return hsv;
 }
 
